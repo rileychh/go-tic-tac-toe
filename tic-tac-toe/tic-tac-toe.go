b87d121ccc8b7b62be8b89a91c4055036e4a9fba
Chenghsuan Ho <chengxuanjhe@gmail.com>
2024-02-15 16:45:03 +0800

    feat: implement taking turns

--- tic-tac-toe/tic-tac-toe.go
+++ tic-tac-toe/tic-tac-toe.go
@@ -2,37 +2,62 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"strings"
 )
+
+func scanlnUntilSuccess(a ...any) (n int) {
+	for attempt := 0; attempt < 5; attempt++ {
+		var err error
+		n, err = fmt.Scanln(a...)
+		if err == nil {
+			break
+		}
+		fmt.Printf("Looks like there's an problem getting your input: %v. Please try again.", err)
+	}
+	return n
+}
 
 func main() {
 	fmt.Println("Welcome to the tic-tac-toe game!")
 
+	var userMark, computerMark cell
+chooseMark:
 	for {
 		fmt.Print("Do you want to play as circle (O) or cross (X)? [O/X] ")
 
 		var userInput string
-		_, err := fmt.Scanln(&userInput)
+		scanlnUntilSuccess(&userInput)
 		userInput = strings.ToUpper(userInput)
-		if userInput == "O" || userInput == "X" {
-			break
-		} else if err != nil {
-			fmt.Printf("Looks like there's an problem getting your input: %v.", err)
-		} else {
-			fmt.Print("You entered something I can't understand.")
+
+		switch userInput {
+		case "O":
+			userMark = circle
+			computerMark = cross
+			break chooseMark
+		case "X":
+			userMark = cross
+			computerMark = circle
+			break chooseMark
+		default:
+			fmt.Println("You entered something I can't understand. Please try again.")
 		}
-		fmt.Println(" Please try again.")
 	}
 
-	for i, row := range board {
-		for j := range row {
-			if (i*len(row)+j)%2 == 0 {
-				board[i][j] = circle
-			} else {
-				board[i][j] = cross
-			}
-		}
-	}
-	fmt.Println(board)
+	fmt.Printf("You'll be playing as %v, and the computer will play as %v.\n", userMark, computerMark)
+
 	var board ticTacToeBoard
+	var isUserTurn = rand.IntN(2) == 0
+
+	fmt.Print(board)
+	for {
+		if isUserTurn {
+			fmt.Print("It's your turn. Enter a coordinate: ")
+		} else {
+			fmt.Println("It's the computer's turn.")
+		}
+
+		isUserTurn = !isUserTurn
+		fmt.Print(board)
+	}
 }
